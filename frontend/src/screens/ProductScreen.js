878cc6264210193b9caa709f70814cc808439dbb
xinyuannn <xz3065@nyu.edu>
2021-11-07 22:25:08 -0500

    fixing a bug

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -30,7 +30,7 @@ const ProductScreen = ({ match }) => {
                         <h3>{product.name}</h3>
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        <Rating value={product.rating} text={'${product.numReviews} reviews'} />
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                     </ListGroup.Item>
                     <ListGroup.Item>
                         Price: ${product.price}
